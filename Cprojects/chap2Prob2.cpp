c7a048c51e1d4c153c7a705da047035fee95c491
boxa72 <craighinkley@live.com>
2021-06-19 20:13:28 +1000

    Arbitrary commit

---
+++ Cprojects/chap2Prob2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    for (int row = 1; row <= 7; row++)
+    {
+        for (int hashNum = 1; hashNum <= 4 - abs(4 - row); hashNum++)
+        {
+            cout << "#";
+        }
+        cout << "\n";
+    }
+    
+}
